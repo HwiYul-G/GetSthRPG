3464f8c513ace92b64410a5082dc92f208cb816f
Y <hwiyul_g@daum.net>
2022-06-10 01:23:45 +0900

    0610

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -13,7 +13,7 @@ public class CameraMovement : MonoBehaviour
 
     void Start()
     {
-        
+        this.transform.position = new Vector3(target.position.x,target.position.y, this.transform.position.z);
     }
 
 
---
+++ Assets/Scripts/Objects/Interactable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public Signal context;
+    public bool playerInRange;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            context.Raise();
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            context.Raise();
+            playerInRange = false;
+        }
+    }
+}
